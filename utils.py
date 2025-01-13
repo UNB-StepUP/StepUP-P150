645bd07970eaac71c0e65c8fbd48b463957fa96a
UNB-StepUP <htl@unb.ca>
2025-01-13 17:26:20 -0400

    Fix comment for clarity

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pathlib
 import numpy as np
 import pandas as pd
 
-dataset_folder = pathlib.Path('/home/shared/stepUP-P150/py') # path to local python folder
+dataset_folder = pathlib.Path('/home/shared/stepUP-P150/py') # path to local data folder
 
 
 def load_metadata(participant_id, footwear, speed):
@@ -11,7 +11,7 @@ def load_metadata(participant_id, footwear, speed):
 
 
 def load_trial(participant_id, footwear, speed):
-  f = dataset_folder / f'{participant_id:03}' / footwear / speed / 'trial.npz'
+  f = dataset_folder / f'{participant_id:03}' / footwear / speed / 'trial.npz'        
   return np.load(f)['arr_0']
 
 
