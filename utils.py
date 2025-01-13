3abe042057c3ab203077249d094e44f68a00eb2f
UNB-StepUP <htl@unb.ca>
2025-01-13 17:15:26 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,16 +2,7 @@ import pathlib
 import numpy as np
 import pandas as pd
 
-dataset_folder = pathlib.Path('/home/shared/stepUP-P150')
-
-
-participant_ids = range(1, 151)
-footwears = ['BF', 'ST', 'P1', 'P2']
-speeds = ['W1', 'W2', 'W3', 'W4']
-
-standing_speeds = ['S1', 'S2', 'S3']
-
-
+dataset_folder = pathlib.Path('/home/shared/stepUP-P150/py') # path to local python folder
 
 
 def load_metadata(participant_id, footwear, speed):
@@ -24,18 +15,6 @@ def load_trial(participant_id, footwear, speed):
   return np.load(f)['arr_0']
 
 
-def load_footsteps(participant_id, footwear, speed):
-  f = dataset_folder / f'{participant_id:03}' / footwear / speed / 'XXXXX.npz'
+def load_footsteps(participant_id, footwear, speed, pipeline = 1):
+  f = dataset_folder / f'{participant_id:03}' / footwear / speed / f'pipeline_{pipeline}.npz'
   return np.load(f)['arr_0']
-
-
-def create_trial_iterator(include_standing_trials=False):
-  included_speeds = speeds.copy()
-  if include_standing_trials:
-    included_speeds.extend(standing_speeds)
-  
-  for p in participant_ids:
-    for f in footwears:
-      for s in included_speeds:
-        yield (p, f, s)
-
